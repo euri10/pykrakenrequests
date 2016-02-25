49707090a65cfeda1d556019d194719febcdb630
Benoit Barthelet <benoit.barthelet@gmail.com>
2016-02-25 09:35:40 +0100

    trade balance updated and tested

--- pykrakenrequests/kprivate.py
+++ pykrakenrequests/kprivate.py
@@ -4,8 +4,13 @@ def kprivate_getBalance(client):
     c = client._post("/0/private/Balance")
     return c['result']
 
-def kprivate_getTradeBalance(client):
-    c = client._post("/0/private/TradeBalance")
+def kprivate_getTradeBalance(client, aclass='currency', asset='ZUSD'):
+    params = {}
+    if aclass:
+        params['aclass']=aclass
+    if asset:
+        params['asset']=asset
+    c = client._post("/0/private/TradeBalance", params)
     return c['result']
 
 def kprivate_getOpenOrders(client, trades=False, userref=None):
--- tests/test_client.py
+++ tests/test_client.py
@@ -41,7 +41,11 @@ class ClientTest(unittest.TestCase):
     def test_trade_balance(self):
         client = pykrakenrequests.Client(key=API_KEY, private_key=PRIVATE_KEY, requests_kwargs=PROXY)
         t = client.kprivate_getTradeBalance()
-        print(t)
+        print(t.keys())
+        # ml should be in this list, dunno why on my account it's not, maybe because i don't have margin yet
+        tbkeys = ['eb', 'tb', 'm', 'n', 'c', 'v', 'e', 'mf' ]
+        tbbool = [k in t.keys() for k in tbkeys]
+        self.assertTrue(all(tbbool))
 
     def test_open_orders(self):
         client = pykrakenrequests.Client(key=API_KEY, private_key=PRIVATE_KEY, requests_kwargs=PROXY)
